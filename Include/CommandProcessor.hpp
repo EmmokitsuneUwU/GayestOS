8fd3152dcb15462310e986b15a931d59942e3f31
EmmokitsuneUwU <emmokitsune@gmail.com>
2025-05-20 15:58:02 -0300

    Delete Include directory

--- Include/CommandProcessor.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef COMMANDPROCESSOR_HPP
-#define COMMANDPROCESSOR_HPP
-#define MAX_ARGS 5
-
-extern void proccessCommand(char *args[MAX_ARGS],int argc);
-extern void parse_command(char *input, char *args[], int *argc);
-
-#endif // COMMANDPROCESSOR_HPP
